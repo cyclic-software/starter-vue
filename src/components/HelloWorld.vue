69a0c3b8bc3d3f3219a11c393284d341050b8f5f
Mei.N <kanakae187@gmail.com>
2023-03-14 00:44:47 +0900

    feat:ダークモード追加（吹き出しのニョロがうまく変わらない）

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -4,28 +4,54 @@
     color="teal-darken-4"
     dark>
     <v-btn variant="text" icon="mdi-menu"></v-btn>
-    <v-app-bar-title>Title</v-app-bar-title>
+    <v-app-bar-title>HatoChan!</v-app-bar-title>
+    <template v-slot:append>
+      <v-switch
+        v-model="darkTheme"
+        @update:model-value="changeTheme"
+        :prepend-icon="darkTheme ? 'mdi-weather-night' : 'mdi-weather-sunny'"
+        hide-details
+        inset
+        class="mr-auto"
+      />
+    </template>
   </v-app-bar>
   <section class="nes-container">
     <section class="message-list">
       <section class="message -left">
         <i class="nes-mario"></i>
         <!-- Balloon -->
-        <div class="nes-balloon from-left">
+        <div class="nes-balloon from-left" style="background-color: transparent;">
           <p>Hello NES.css</p>
         </div>
       </section>
 
       <section class="message -right">
         <!-- Balloon -->
-        <div class="nes-balloon from-right">
+        <div class="nes-balloon from-right" style="background-color: transparent;">
           <p>Good morning. Thou hast had a good night's sleep, I hope.</p>
         </div>
         <i class="nes-kirby"></i>
       </section>
     </section>
   </section>
-
+  <v-btn @click="test">ぼたん</v-btn>
 </template>
+
+<script setup>
+import { ref } from 'vue'
+import { useTheme } from 'vuetify'
+
+const darkTheme = ref(false)
+const theme = useTheme()
+
+const changeTheme = () => {
+  theme.global.name.value = darkTheme.value ? 'dark' : 'light'
+}
+
+const test = () => {
+  console.log("押した")
+}
+</script>
 
 
