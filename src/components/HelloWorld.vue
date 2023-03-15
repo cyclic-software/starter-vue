d00ec99a9daf97697446ea5a595206a3d01a010c
Mei.N <kanakae187@gmail.com>
2023-03-15 22:56:02 +0900

    feat:記事の目次に使用できそうなリストを追加

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -29,25 +29,32 @@
       />
     </template>
   </v-app-bar>
-  <section class="nes-container">
-    <section class="message-list">
-      <section class="message -left">
-        <i class="nes-mario"></i>
-        <!-- Balloon -->
-        <div class="nes-balloon from-left" style="background-color: transparent;">
-          <p>Hello NES.css</p>
-        </div>
-      </section>
+  <v-row>
+    <v-col>
+      <section class="nes-container">
+        <section class="message-list">
+          <section class="message -left">
+            <i class="nes-mario"></i>
+            <!-- Balloon -->
+            <div class="nes-balloon from-left" style="background-color: transparent;">
+              <p>Hello NES.css</p>
+            </div>
+          </section>
 
-      <section class="message -right">
-        <!-- Balloon -->
-        <div class="nes-balloon from-right" style="background-color: transparent;">
-          <p>Good morning. Thou hast had a good night's sleep, I hope.</p>
-        </div>
-        <i class="nes-kirby"></i>
+          <section class="message -right">
+            <!-- Balloon -->
+            <div class="nes-balloon from-right" style="background-color: transparent;">
+              <p>Good morning. Thou hast had a good night's sleep, I hope.</p>
+            </div>
+            <i class="nes-kirby"></i>
+          </section>
+        </section>
       </section>
-    </section>
-  </section>
+    </v-col>
+    <v-col cols="3">
+      <ArticleList />
+    </v-col>
+  </v-row>
   <v-btn @click="test">ぼたん</v-btn>
   <SoundPlayer number=1></SoundPlayer>
   <ApiTest></ApiTest>
@@ -56,7 +63,7 @@
 
 
 <script setup>
-
+import ArticleList from './component/ArticleList.vue'
 
 import { ref } from 'vue'
 import { useTheme } from 'vuetify'
---
+++ src/components/component/ArticleList.vue
@@ -0,0 +1,15 @@
+<template>
+  <div style="padding: 0 20px 20px 0;">
+    <v-card>
+      <v-list :items="items"></v-list>
+    </v-card>
+  </div>
+</template>
+
+<script setup>
+const items = [
+  { title: 'content1' },
+  { title: 'content2' },
+  { title: 'content3' },
+]
+</script>
